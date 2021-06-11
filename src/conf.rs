b08b8ad1d2b2ecec33b790e80f05fc95e9c7ae19
Felix Hennig <mail@felixhennig.com>
2021-06-11 14:46:25 +0200

    Added config struct

--- src/conf.rs
+++ src/conf.rs
@@ -1,7 +1,9 @@
-use yaml_rust::YamlLoader;
+use crate::osc_handler::OscPath;
+use crate::piblaster::GpioPin;
+use anyhow::{bail, Result};
 use log::info;
 use std::fs;
-use anyhow::Result;
+use yaml_rust::{Yaml, YamlLoader};
 
 pub fn load_config() -> Result<()> {
     let config_raw = fs::read_to_string("conf.yaml")?;
@@ -9,4 +11,21 @@ pub fn load_config() -> Result<()> {
     let doc = &docs[0];
     info!("{:?}", doc);
     Ok(())
+}
+
+pub struct Config {
+    yaml: Yaml,
+}
+
+impl Config {
+    pub fn new() -> Result<Self> {
+        let config_raw = fs::read_to_string("conf.yaml")?;
+        let mut docs = YamlLoader::load_from_str(&config_raw)?;
+        if docs.len() != 0 {
+            bail!("Only a single doc is supported.");
+        }
+        let doc = docs.remove(0);
+        info!("{:?}", doc);
+        Ok(Self { yaml: doc })
+    }
 }
